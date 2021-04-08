3cbb20c7721e436139febb813259cbf8aa59a571
Joberth Rogers <joberth.rogers18@gmail.com>
2021-04-08 11:47:28 -0300

    feat: add bcm2835 lib to deal with gpio

---
+++ projeto-2-2020.2/server_distributed/inc/gpio.h
@@ -0,0 +1,9 @@
+#ifndef GPIO_H_
+#define GPIO_H_
+
+void initPinsGPIO();
+void turnOnGPIO();
+void turnOffGPIO();
+void closeGPIO();
+
+#endif
--- projeto-2-2020.2/server_distributed/src/main.c
+++ projeto-2-2020.2/server_distributed/src/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <read_bme280.h>
+#include <gpio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 #define ADDRESS_BME280 0x76
 #define CHANNEL_BME280 1
@@ -12,17 +15,28 @@ void initResources() {
 	if (i != 0)
 	{
 		printf("Erro to open\n");
+        exit(1);
 	}
 }
 
 int main(int argc, char const *argv[])
 {
-    initResources();
-    getInformationBME280(&temperatureExternal, &humidity);
+    // initResources();
+    // getInformationBME280(&temperatureExternal, &humidity);
 
-    float temperatureExt = (float)temperatureExternal/100;
-    float humidityParser = (float)humidity/1000;
-    printf("Temperature: %.2f Humidity: %.2f\n", temperatureExt, humidityParser);
+    // float temperatureExt = (float)temperatureExternal/100;
+    // float humidityParser = (float)humidity/1000;
+    // printf("Temperature: %.2f Humidity: %.2f\n", temperatureExt, humidityParser);
+
+    initPinsGPIO();
+
+    turnOnGPIO();
+
+    sleep(10);
+
+    turnOffGPIO();
+
+    closeGPIO();
 
     return 0;
 }
